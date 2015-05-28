81ed5889b6434675e3f4d428d1c8c3d61dbabb1a
DQ4IoT <jgael@ether2.com>
2015-05-28 11:48:58 -0700

    Create byte.h

---
+++ byte.h
@@ -0,0 +1,10 @@
+#ifndef MAXSTATIONS
+#define MAXSTATIONS 100
+#endif
+
+typedef unsigned char byte;
+
+long random(long __num);
+void cadcpy (char *destino, char *origen);
+void cadcat (char *destino, char *origen);
+int cadlen (char *cadena);
